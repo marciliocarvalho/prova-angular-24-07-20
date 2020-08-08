9e0713f1b63f3de75ab6294ebb51fa82aedc6c42
Marcilio Carvalho <marcilioc108@gmail.com>
2020-08-08 19:44:45 -0300

    Copiando do app.component.* para o list-person.*

--- src/app/list-person/list-person.component.ts
+++ src/app/list-person/list-person.component.ts
@@ -7,9 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListPersonComponent implements OnInit {
 
+  persons = [
+    {
+      name: 'Teste',
+      cpf: 'Teste',
+      phone: 'Teste',
+      email: 'Teste',
+      cep: 'Teste',
+      state: 'Teste',
+      city: 'Teste',
+      street: 'Teste',
+    }
+  ];
+  columns = ['name', 'cpf', 'phone', 'email', 'cep', 'state', 'city', 'street', 'actions'];
+  selectedPerson;
+
   constructor() { }
 
   ngOnInit() {
   }
+
+  editPerson(person) {
+		this.selectedPerson = { ...person }
+  }
+  
+  deletePerson(person) {
+		remove(person)
+		this.persons = JSON.parse(get())
+	}
 
 }
